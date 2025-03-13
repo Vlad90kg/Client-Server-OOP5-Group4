1a5e7924baa211dde3956b748b4bf4255e640242
vladikslon <vladbrovi@gmail.com>
2025-03-13 22:32:06 +0000

    Merge remote-tracking branch 'origin/Ben'

--- src/main/java/group4/group4/Main.java
+++ src/main/java/group4/group4/Main.java
@@ -7,22 +7,44 @@ import group4.group4.dto.MobilePhone;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws DaoException {
         DaoMobilePhone daoMobilePhone = new DaoMobilePhoneImpl();
-
+        Scanner scanner = new Scanner(System.in);
         Comparator<MobilePhone> comparator =  (p1, p2) -> Double.compare(p1.getPrice(), p2.getPrice());
         List<MobilePhone> list = daoMobilePhone.getAll();
-        List<MobilePhone> phonesOver500 = daoMobilePhone.findByFilter(comparator);
+        List<MobilePhone> phonesOverX = daoMobilePhone.findByFilter(comparator);
         for (MobilePhone mobilePhone : list) {
             System.out.println(mobilePhone);
         }
-        System.out.println("Phones over 500: ");
 
-        for (MobilePhone mobilePhone : phonesOver500) {
+        System.out.println("Phones over X: ");
+
+
+        for (MobilePhone mobilePhone : phonesOverX) {
             System.out.println( mobilePhone);
-            System.out.println(mobilePhone);
         }
+        System.out.println("Enter Brand ID to delete: ");
+
+        int brand_id = scanner.nextInt();
+        scanner.nextLine();
+        daoMobilePhone.delete(brand_id);
+        System.out.println("Brand id deleted successfully!");
+
+
+        System.out.print("Enter the Brand ID to search: ");
+        int id = scanner.nextInt();
+
+        MobilePhone phone = daoMobilePhone.getById(id);
+
+        if (phone != null) {
+            System.out.println("Brand id found:");
+            System.out.println(phone);
+        } else {
+            System.out.println("No Phone found with Brand ID: " + id);
+        }
+
     }
 }
