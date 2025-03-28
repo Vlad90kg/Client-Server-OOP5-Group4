59f830a5f2d1682f95a95a255b0330102cb02da9
vladikslon <vladbrovi@gmail.com>
2025-03-28 23:46:31 +0000

    Implemented feature 10 client side: Implement a client-side menu option “Display all Entities” that will send a request to the server to obtain a list of all entities

--- src/main/java/group4/group4/client/Main.java
+++ src/main/java/group4/group4/client/Main.java
@@ -4,7 +4,13 @@ import group4.group4.Exceptions.DaoException;
 import group4.group4.server.dao.DaoMobilePhone;
 import group4.group4.server.dao.DaoMobilePhoneImpl;
 import group4.group4.server.dto.MobilePhone;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
@@ -14,195 +20,94 @@ public class Main {
 
     public static void main(String[] args) throws DaoException {
         Main mainInstance = new Main();
-        DaoMobilePhone daoMobilePhone = new DaoMobilePhoneImpl();
-
-        boolean exit = false;
-
-        while (!exit) {
-            System.out.println("=== Mobile Phone Management System ===");
-            System.out.println("1. Display All Phones");
-            System.out.println("2. Search Phone by ID");
-            System.out.println("3. Delete Phone by ID");
-            System.out.println("4. Insert Phone");
-            System.out.println("5. Update Phone");
-            System.out.println("6. Filter Phones by Price");
-            System.out.println("7. Exit");
-            System.out.print("Enter your choice: ");
-
-            int choice = 0;
-            try {
-                choice = Integer.parseInt(scanner.nextLine());
-            } catch (NumberFormatException e) {
-                System.out.println("Invalid input. Please enter a number.");
-                continue;
-            }
-
-            switch (choice) {
-                case 1:
-//                    mainInstance.getAllPhones(daoMobilePhone);
-                    break;
-                case 2:
-                    mainInstance.getPhoneById(daoMobilePhone);
-                    break;
-                case 3:
-                    mainInstance.deletePhoneById(daoMobilePhone);
-                    break;
-                case 4:
-                    MobilePhone insertedPhone = mainInstance.insert(daoMobilePhone);
-                    System.out.println("Inserted Phone: " + insertedPhone);
-                    break;
-                case 5:
-                    mainInstance.update(daoMobilePhone);
-                    break;
-                case 6:
-                    List<MobilePhone> filteredPhones = mainInstance.getFilteredPhones(daoMobilePhone);
-                    System.out.println("Filtered Phones by Price:");
-                    for (MobilePhone phone : filteredPhones) {
-                        System.out.println(phone);
-                    }
-                    break;
-                case 7:
-                    exit = true;
-                    System.out.println("Exiting...");
-                    break;
-                default:
-                    System.out.println("Invalid choice. Please try again.");
-            }
-
-            System.out.println();
-        }
-
+        mainInstance.start(8080);
     }
 
-    public List<MobilePhone> getAllPhones(DaoMobilePhone daoMobilePhone) throws DaoException {
-        // Feature 1
-        List<MobilePhone> list = daoMobilePhone.getAll();
-        for (MobilePhone mobilePhone : list) {
-            System.out.println(mobilePhone);
-        }
-        return list;
-    }
+    public void start(int port)  {
+        try(Socket socket = new Socket("localhost", port);
+            OutputStream outputStream = socket.getOutputStream();
+        ) {
+            PrintWriter out = new PrintWriter(outputStream, true);
+            System.out.println("The client is running and has connected to the server.");
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            boolean exit = false;
+            while (!exit) {
+                System.out.println("=== Mobile Phone Management System ===");
+                System.out.println("1. Display All Phones");
+                System.out.println("2. Search Phone by ID");
+                System.out.println("3. Delete Phone by ID");
+                System.out.println("4. Insert Phone");
+                System.out.println("5. Update Phone");
+                System.out.println("6. Filter Phones by Price");
+                System.out.println("7. Exit");
+                System.out.print("Enter your choice: ");
 
-    public MobilePhone getPhoneById(DaoMobilePhone daoMobilePhone) throws DaoException {
-        // Feature 2
-        System.out.print("Enter the ID to search: ");
-        int id = scanner.nextInt();
-
-        MobilePhone phone = daoMobilePhone.getById(id);
-
-        if (phone != null) {
-            System.out.println("Id found:");
-            System.out.println(phone);
-        } else {
-            System.out.println("No Phone found with ID: " + id);
-        }
-        return phone;
-    }
-
-    public void deletePhoneById(DaoMobilePhone daoMobilePhone) throws DaoException {
-        // Feature 3
-        System.out.println("Enter ID to delete: ");
-        boolean validId = false;
-        int id = -1;
-
-        while (!validId) {
-            try {
-                id = Integer.parseInt(scanner.nextLine());
-                if (id >= 0) {
-                    validId = true;
+                int choice = 0;
+                try {
+                    choice = Integer.parseInt(scanner.nextLine());
+                } catch (NumberFormatException e) {
+                    System.out.println("Invalid input. Please enter a number.");
+                    continue;
                 }
-            } catch (NumberFormatException e) {
-                System.out.println("Invalid input. Please enter a number.");
-            }
-            System.out.println("Enter valid ID to delete: ");
-        }
-        scanner.nextLine();
-        int rowsAffected = daoMobilePhone.delete(id);
-        if (rowsAffected > 0) {
-            System.out.println("Phone with ID " + id + " deleted successfully!");
-        }
-        else {
-            System.out.println("No phone found with ID " + id);
-        }
-    }
+                System.out.println();
+                switch (choice) {
+                    case 1:
+                        String getAllString = "getAll";
+                        out.println(getAllString);
+                        String response = in.readLine();
+                        JSONArray jsonArray = new JSONArray(response);
+                        List<MobilePhone> mobilePhones = new ArrayList<>();
+                        for (int i = 0; i < jsonArray.length(); i++) {
+                            JSONObject jsonObject = jsonArray.getJSONObject(i);
+                            int id = jsonObject.getInt("id");
+                             int brandId = jsonObject.getInt("brandId");
+                             String model = jsonObject.getString("model");
+                             int quantity = jsonObject.getInt("quantity");
+                             double price  = jsonObject.getDouble("price");
+                             mobilePhones.add(new MobilePhone(id, brandId, model, quantity, price));
+                        }
+                        for (MobilePhone mobilePhone : mobilePhones) {
+                            System.out.println(mobilePhone);
+                        }
 
-    public MobilePhone insert(DaoMobilePhone daoMobilePhone) throws DaoException {
-        // Feature 4
-        String newModel = "";
-        int newBrandId = 0, newQuantity = 0;
-        double newPrice = 0;
-        boolean validInput = false;
-        while (!validInput) {
-            try {
-                System.out.print("Enter brand ID: ");
-                newBrandId = Integer.parseInt(scanner.nextLine());
-                System.out.print("Enter model: ");
-                newModel = scanner.nextLine();
-                System.out.print("Enter quantity: ");
-                newQuantity = Integer.parseInt(scanner.nextLine());
-                System.out.print("Enter price: ");
-                newPrice = Double.parseDouble(scanner.nextLine());
-                if (newBrandId > 0 || !newModel.isEmpty() || newQuantity >= 0 || newPrice > 0) {
-                    validInput = true;
+
+                        break;
+                    case 2:
+//                        mainInstance.getPhoneById(daoMobilePhone);
+                        break;
+                    case 3:
+//                        mainInstance.deletePhoneById(daoMobilePhone);
+                        break;
+                    case 4:
+//                        MobilePhone insertedPhone = mainInstance.insert(daoMobilePhone);
+//                        System.out.println("Inserted Phone: " + insertedPhone);
+                        break;
+                    case 5:
+//                        mainInstance.update(daoMobilePhone);
+                        break;
+                    case 6:
+//                        List<MobilePhone> filteredPhones = mainInstance.getFilteredPhones(daoMobilePhone);
+//                        System.out.println("Filtered Phones by Price:");
+//                        for (MobilePhone phone : filteredPhones) {
+//                            System.out.println(phone);
+//                        }
+                        break;
+                    case 7:
+                        exit = true;
+                        out.println("exit");
+                        System.out.println("Exiting...");
+                        break;
+                    default:
+                        System.out.println("Invalid choice. Please try again.");
                 }
-
-            } catch (NumberFormatException e) {
-                System.out.println("Invalid input. Please enter a number.");
             }
-            if (!validInput) {
-                System.out.println("Invalid input. Please try again.");
-            }
+
         }
-        return daoMobilePhone.insert(new MobilePhone(newBrandId, newModel, newQuantity, newPrice));
-
-    }
-
-    public void update(DaoMobilePhone daoMobilePhone) throws DaoException {
-        // Feature 5
-        String updatedModel = "";
-        int idToUpdate = 0, updatedBrandId = 0, updatedQuantity = 0;
-        double updatedPrice = 0;
-
-        boolean validInput = false;
-        while (!validInput) {
-            try {
-                System.out.print("Enter ID of mobile phone you would like to change: ");
-                idToUpdate = Integer.parseInt(scanner.nextLine());
-                System.out.print("Enter new brand ID: ");
-                updatedBrandId = Integer.parseInt(scanner.nextLine());
-                System.out.print("Enter new model: ");
-                updatedModel = scanner.nextLine();
-                System.out.print("Enter new quantity: ");
-                updatedQuantity = Integer.parseInt(scanner.nextLine());
-                System.out.print("Enter new price: ");
-                updatedPrice = Double.parseDouble(scanner.nextLine());
-
-                if (idToUpdate >= 0 || updatedBrandId > 0 || !updatedModel.isEmpty() || updatedQuantity >= 0 || updatedPrice > 0) {
-                    validInput = true;
-                }
-            } catch (NumberFormatException e) {
-                System.out.println("Invalid input. Please enter a number.");
-            }
-            if (!validInput) {
-                System.out.println("Invalid input. Please try again.");
-            }
+        catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-
-        int rowsAffected = daoMobilePhone.update(idToUpdate, new MobilePhone(updatedBrandId, updatedModel, updatedQuantity, updatedPrice));
-        if (rowsAffected > 0) {
-            System.out.println("Phone with ID " + idToUpdate + " updated successfully!");
-        }
-        else {
-            System.out.println("No phone found with ID " + idToUpdate);
-        }
-
-    }
-
-
-    public List<MobilePhone> getFilteredPhones(DaoMobilePhone daoMobilePhone) throws DaoException {
-        // Feature 6
-        Comparator<MobilePhone> comparator = (p1, p2) -> Double.compare(p1.getPrice(), p2.getPrice());
-        return daoMobilePhone.findByFilter(comparator);
     }
 
 }
